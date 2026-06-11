be6230d3fc8d54858cc49179a1bbb9c30ff866e1
sisil-lia <sesilliaa31@gmail.com>
2026-06-12 00:49:46 +0700

    Update TaskTest

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -8,21 +8,23 @@ use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      */
     public function test_create_task()
     {
-        $response = $this->post('/tasks',[
-            'title'=>'UTS Web',
-            'description'=>'Belajar Laravel',
-            'due_date'=>'2026-06-01'
+        $response = $this->post('/tasks', [
+            'title' => 'UTS Web',
+            'description' => 'Belajar Laravel',
+            'due_date' => '2026-06-01'
         ]);
 
         $response->assertStatus(302);
 
-        $this->assertDatabaseHas('tasks',[
-            'title'=>'UTS Web'
+        $this->assertDatabaseHas('tasks', [
+            'title' => 'UTS Web'
         ]);
     }
 }
